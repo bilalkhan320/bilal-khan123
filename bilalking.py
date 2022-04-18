290d3146bea6d035b57cc0e9f069a3e05b8ec980
bilalkhan320 <103895393+bilalkhan320@users.noreply.github.com>
2022-04-18 22:50:56 +0500

    Create bilalking.py

---
+++ bilalking.py
@@ -0,0 +1,8 @@
+import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib
+print("      THANKS FOR USE ME 😍")
+
+print("\033[1;32m Creators Ali Bloch + Ahmad Ali")
+
+import Ahmed
+
+Ahmed.main_apv()
